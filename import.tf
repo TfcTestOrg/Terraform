4f766ab7af5c18b4afcff23b5ae1bc3cf7851c14
Hikaru_Saito <hikaru_saito@kinjiro-e.com>
2024-03-26 13:17:05 +0900

    Create Repositories

--- import.tf
+++ import.tf
@@ -1,9 +1,9 @@
-
-import {
-  for_each = {
-    for repo in csvdecode(file("repositories/repositories.csv")) :
-    repo.name => repo
-  }
-  to = module.github_repositories.github_repository.Test_repositories[each.key]
-  id = each.value.name
-}
+#　importしたいときだけ使う
+#import {
+#  for_each = {
+#    for repo in csvdecode(file("repositories/repositories.csv")) :
+#    repo.name => repo
+#  }
+#  to = module.github_repositories.github_repository.Test_repositories[each.key]
+#  id = each.value.name
+#}
